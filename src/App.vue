9c6a245f40ef91eb6691edb209d18cf7be719b56
Alex <alessandro.piccione.75@gmail.com>
2025-09-26 17:26:44 +0200

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-contaiuner">
     <img v-if="!isLoggedIn" src="/logo.png" class="logo" alt="Portfolio logo" />
-    <Toolbar_2 v-if="isLoggedIn" />
+    <Toolbar v-if="isLoggedIn" />
     <router-view />
   </div>
 </template>
@@ -9,25 +9,15 @@
 <style scoped lang="scss">
 @use "styles/theme" as theme;
 
-.__app-container {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-}
-
-/* Add padding to the top to account for the fixed toolbar */
-:deep(.router-view-wrapper) {  // Use deep selector
-  padding-top: 40px; /* Adjust value as needed based on toolbar height */
-}
-
 .logo {
   height: 12rem;
   padding: 1.5rem;
   will-change: filter;
   transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2rem theme.$primary-color);
+
+  &:hover {
+   filter: drop-shadow(0 0 2rem theme.$primary-color);
+ }
 }
 </style>
 
@@ -36,8 +26,6 @@ import { computed, onMounted, provide } from 'vue'
 import { goTo } from './utils/router'
 import { useAuthStore } from './stores/auth.store'
 import Toolbar from './components/Toolbar.vue'
-import Toolbar_2 from './components/Toolbar_2.vue'
-
 
 /*
 const authService = new AuthService()
